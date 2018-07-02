ce8258ea150aff435243658f8710ab9edfbe5aea
Alexey Khrusch <alexeykhr@outlook.com>
2018-07-02 20:36:31 +0300

    Install basic webpack (dependencies, configuration)

---
+++ dist/1520912f620a6b92fb08601f2b3504e8.js
@@ -0,0 +1,6 @@
+import jQuery from 'jquery'
+import 'font-awesome/css/font-awesome.css'
+import 'normalize.css'
+import './css/style.css'
+
+window.$ = window.jQuery = jQuery
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader'
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader'
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              publicPath: 'dist/'
+            }
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              disable: true
+            }
+          }
+        ]
+      }
+    ]
+  }
+}
