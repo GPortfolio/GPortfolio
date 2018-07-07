92a714665f1800524ed98be70e91a4611e76feff
Alexey Khrushch <alexeykhr@outlook.com>
2018-07-07 19:01:26 +0300

    Configurated webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,26 @@
 'use strict'
+const HtmlWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'development',
   entry: ['./src/js/main.js', './src/scss/main.scss'],
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: __dirname,
     filename: '[name].js'
   },
   module: {
     rules: [
       {
-        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'url-loader'
-      },
-      {
-        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        test: /\.(ttf|eot|svg|woff(2)?)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
         loader: 'url-loader'
       },
       {
         test: /\.(gif|png|jpe?g|svg)$/i,
         use: [
           {
-            loader: 'file-loader',
-            options: {
-              // publicPath: 'dist/'
-            }
+            loader: 'file-loader'
           },
           {
             loader: 'image-webpack-loader',
@@ -39,15 +33,24 @@ module.exports = {
       {
         test: /\.scss/,
         use: [
-          'css-hot-loader',
           MiniCssExtractPlugin.loader,
           'css-loader',
           'sass-loader'
         ]
+      },
+      {
+        test: /\.js$/,
+        loader: "babel-loader"
       }
     ]
   },
   plugins: [
-    new MiniCssExtractPlugin('[name].css')
+    new MiniCssExtractPlugin('[name].css'),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html',
+      title: 'Test'
+    }),
+    new HtmlWebpackHarddiskPlugin()
   ]
 }
