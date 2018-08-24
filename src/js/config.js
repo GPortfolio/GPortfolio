147852772e7ee708fd6ed9d94522bea2d82083f7
Alexey Khrusch <alexeykhr@outlook.com>
2018-08-24 12:32:23 +0300

    Update header

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
+const packageJSON = require('../../package')
+
 module.exports = {
   // PWA
   app_icon: 'static/images/profile/avatar.png',
@@ -11,7 +13,16 @@ module.exports = {
   email: 'alexeykhr@outlook.com',
   profession: 'Web developer',
   image: 'static/images/profile/avatar.png',
-  about: '',
+  github_url: packageJSON.homepage,
+  about_me: [
+    'Hi, my name is Alexey Khrushch, I\'m from Ukraine. Now I working in Semalt company, studying in KNUTE university ' +
+    'and improving my knowledge on their projects in Github. I like developing my programming skills and I\'m not ' +
+    'afraid of difficulties. You also can view source code this site on Github repository.',
+    'Love: IT, Traveling, Sport and Open Source.'
+  ],
+  skills: [
+    'PHP', 'HTML', 'CSS', 'Javascript', 'jQuery', 'SQL', 'Git', 'Laravel', 'Vue.js', 'C#'
+  ],
   social_media: [
     { name: 'Twitter', href: 'https://twitter.com/Alexeykhr_', icon: 'twitter' },
     { name: 'LinkedIn', href: 'https://www.linkedin.com/in/alexeykhr', icon: 'linkedin' },
