b02f3318420b1c5cc558f37bd1ec63ebe4460e5d
Alexey Khrusch <alexeykhr@outlook.com>
2018-08-24 13:56:50 +0300

    Update images + text and styles

--- src/js/config.js
+++ src/js/config.js
@@ -1,24 +1,24 @@
 const packageJSON = require('../../package')
 
 module.exports = {
-  // PWA
-  app_icon: 'static/images/profile/avatar.png',
+  // pwa
+  app_icon: 'static/images/pixabay/resume.png',
   app_color: '#fff',
 
-  // USER
+  // profile
   username: 'Alexeykhr',
   first_name: 'Alexey',
   last_name: 'Khrushch',
   gender: 'Male',
   email: 'alexeykhr@outlook.com',
   profession: 'Web developer',
-  image: 'static/images/profile/avatar.png',
+  image: 'static/images/profile/avatar.jpg',
   github_url: packageJSON.homepage,
   about_me: [
     'Hi, my name is Alexey Khrushch, I\'m from Ukraine. Now I working in Semalt company, studying in KNUTE university ' +
     'and improving my knowledge on their projects in Github. I like developing my programming skills and I\'m not ' +
     'afraid of difficulties. You also can view source code this site on Github repository.',
-    'Love: IT, Traveling, Sport and Open Source.'
+    'Love: IT, Traveling, Sport, Open Source and tea.'
   ],
   skills: [
     'PHP', 'HTML', 'CSS', 'Javascript', 'jQuery', 'SQL', 'Git', 'Laravel', 'Vue.js', 'C#'
@@ -28,34 +28,21 @@ module.exports = {
     { name: 'LinkedIn', href: 'https://www.linkedin.com/in/alexeykhr', icon: 'linkedin' },
     { name: 'Github', href: 'https://github.com/Alexeykhr', icon: 'github' }
   ],
-  education: [
-    {
-      name: 'Kyiv national university of trade and economics (KNUTE)',
-      degree: 'Bachelor',
-      specialization: 'Computer Software Engineering',
-      year: {
-        receipt: 2016,
-        ending: 2019
-      }
-    },
-    {
-      name: '',
-      degree: '',
-      specialization: '',
-      year: {
-        receipt: 0,
-        ending: 0
-      }
-    }
-  ],
-  projects: [
-    {
-      name: ''
-    }
-  ],
-  jobs: [
-    {
-      name: ''
-    }
-  ]
+  education: [{
+    name: 'Kyiv national university of trade and economics (KNUTE)',
+    degree: 'Bachelor',
+    specialization: 'Computer Software Engineering',
+    year: { receipt: 2016, ending: 2019 }
+  }, {
+    name: '',
+    degree: '',
+    specialization: '',
+    year: { receipt: 0, ending: 0 }
+  }],
+  projects: [{
+    name: ''
+  }],
+  jobs: [{
+    name: ''
+  }],
 }
