b054f0f70b6795f01a0a2089c9794c10e28886f7
Alexey Khrusch <alexeykhr@outlook.com>
2018-11-11 21:18:19 +0200

    Remove header lite version, update general header

--- src/templates/t2/js/header.js
+++ src/templates/t2/js/header.js
@@ -2,16 +2,12 @@ import { DOMIsLoaded, getOffsetTop } from '../../../scripts/utils'
 
 // DOM elements
 let headerEl = null
-let headerLiteEl = null
 let backgroundImageEl = null
 let headerContentEl = null
 
 // Variables
 let backgroundImageHeight = 0
 let headerHeight = 0
-
-let _timeout = null
-let _isStart = false
 
 /*
  * Functions
@@ -26,40 +22,22 @@ const updateStyles = () => {
   backgroundImageEl.style.height = `calc(100% + ${backgroundImageEl.clientHeight * .2}px)`
 }
 
-/*
- * Window events
- */
-
-const onWindowScroll = (evt) => {
+const updateStylesScroll = () => {
   const offsetTop = getOffsetTop()
 
   if (headerHeight > offsetTop) {
     backgroundImageEl.style.transform = `translateY(-${offsetTop * .2}px)`
     headerContentEl.style.transform = `translateY(${offsetTop * .05}px)`
     headerContentEl.style.opacity = (1 - offsetTop / (backgroundImageHeight * 2))
-
-    if (!_isStart) {
-      _isStart = true
-      headerLiteEl.classList.remove('animation')
-      _timeout = setTimeout(() => {
-        console.log('Remove')
-        headerLiteEl.classList.remove('show')
-      }, 100)
-    }
-  } else {
-    clearTimeout(_timeout)
-    backgroundImageEl.style.transform = null
-    headerContentEl.style.transform = null
-    headerContentEl.style.opacity = null
-
-    if (_isStart) {
-      _isStart = false
-      headerLiteEl.classList.add('show')
-      setTimeout(() => {
-        headerLiteEl.classList.add('animation')
-      })
-    }
   }
+}
+
+/*
+ * Window events
+ */
+
+const onWindowScroll = (evt) => {
+  updateStylesScroll()
 }
 
 const onWindowResize = (evt) => {
@@ -74,13 +52,13 @@ const onWindowResize = (evt) => {
 DOMIsLoaded(() => {
   // Get all elements from DOM
   headerEl = document.querySelector('.header')
-  headerLiteEl = document.querySelector('.header-lite')
   backgroundImageEl = document.querySelector('.background--image')
-  headerContentEl = document.querySelector('.header-full__content')
+  headerContentEl = document.querySelector('.header__content')
 
   // Call all the necessary functions during initialization
   updateVariables()
   updateStyles()
+  updateStylesScroll()
 
   // Register events
   window.addEventListener('scroll', onWindowScroll)
