8641ef795d5402a4e45164d17b48096acf381f6a
Alexey Khrusch <alexeykhr@outlook.com>
2018-11-11 18:28:04 +0200

    Add basic lite version of header

--- src/templates/t2/js/header.js
+++ src/templates/t2/js/header.js
@@ -1,25 +1,88 @@
 import { DOMIsLoaded, getOffsetTop } from '../../../scripts/utils'
 
-let appEl = null
+// DOM elements
+let headerEl = null
+let headerLiteEl = null
+let backgroundImageEl = null
+let headerContentEl = null
 
-const updateAppClass = () => {
-  if (!getOffsetTop()) {
-    appEl.classList.add('app_top')
+// Variables
+let backgroundImageHeight = 0
+let headerHeight = 0
+
+let _timeout = null
+let _isStart = false
+
+/*
+ * Functions
+ */
+
+const updateVariables = () => {
+  backgroundImageHeight = headerContentEl.clientHeight
+  headerHeight = headerEl.clientHeight
+}
+
+const updateStyles = () => {
+  backgroundImageEl.style.height = `calc(100% + ${backgroundImageEl.clientHeight * .2}px)`
+}
+
+/*
+ * Window events
+ */
+
+const onWindowScroll = (evt) => {
+  const offsetTop = getOffsetTop()
+
+  if (headerHeight > offsetTop) {
+    backgroundImageEl.style.transform = `translateY(-${offsetTop * .2}px)`
+    headerContentEl.style.transform = `translateY(${offsetTop * .05}px)`
+    headerContentEl.style.opacity = (1 - offsetTop / (backgroundImageHeight * 2))
+
+    if (!_isStart) {
+      _isStart = true
+      headerLiteEl.classList.remove('animation')
+      _timeout = setTimeout(() => {
+        console.log('Remove')
+        headerLiteEl.classList.remove('show')
+      }, 100)
+    }
   } else {
-    appEl.classList.remove('app_top')
+    clearTimeout(_timeout)
+    backgroundImageEl.style.transform = null
+    headerContentEl.style.transform = null
+    headerContentEl.style.opacity = null
+
+    if (_isStart) {
+      _isStart = false
+      headerLiteEl.classList.add('show')
+      setTimeout(() => {
+        headerLiteEl.classList.add('animation')
+      })
+    }
   }
 }
 
-const onWindowScroll = (evt) => {
-  updateAppClass()
+const onWindowResize = (evt) => {
+  updateVariables()
+  updateStyles()
 }
 
+/*
+ * Main part
+ */
+
 DOMIsLoaded(() => {
-  appEl = document.querySelector('#app')
+  // Get all elements from DOM
+  headerEl = document.querySelector('.header')
+  headerLiteEl = document.querySelector('.header-lite')
+  backgroundImageEl = document.querySelector('.background--image')
+  headerContentEl = document.querySelector('.header-full__content')
 
-  setTimeout(() => {
-    updateAppClass()
-  }, 500)
+  // Call all the necessary functions during initialization
+  updateVariables()
+  updateStyles()
 
+  // Register events
   window.addEventListener('scroll', onWindowScroll)
+  window.addEventListener('resize', onWindowResize)
 })
--- src/templates/t2/js/index.js
+++ src/templates/t2/js/index.js
@@ -1,2 +1,2 @@
+import './app'
 import './header'
-import './headerParallax'
