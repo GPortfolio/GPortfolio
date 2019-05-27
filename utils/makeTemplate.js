10e3f0f380fac44f4a003382ba7134da3ff1de97
Alexey Khruhsch <alexeykhr@outlook.com>
2019-05-27 09:00:01 +0300

    feat: add makeTemplate.js utils

---
+++ utils/makeTemplate.js
@@ -0,0 +1,90 @@
+'use strict'
+
+/* | ------------------------------------------------------------------------------------------------
+ * | - Generate a basic files/content for new template. -
+ * | ------------------------------------------------------------------------------------------------
+ * |
+ * | How to create: npm run template <name>
+ * | Example: npm run template myTemplate
+ * |
+ */
+
+const argv = process.argv.slice(2)
+const path = require('path')
+const fs = require('fs')
+
+// Check name of template
+if (argv.length !== 1) {
+  console.warn('Invalid arguments')
+  process.exit(0)
+}
+
+/**
+ * @var {string}
+ */
+const name = argv[0]
+
+/**
+ * Path to templates
+ * @type {string}
+ */
+const BASE_PATH = path.resolve(__dirname, '../src/templates') + '/'
+
+// Check on exists template
+if (fs.existsSync(BASE_PATH + name)) {
+  console.warn(`[Exists]: ${BASE_PATH + name}`)
+  process.exit(0)
+}
+
+// Create folder
+fs.mkdirSync(BASE_PATH + name)
+
+/*
+ * ---------------------------------------------- Create required .js file
+ */
+fs.writeFileSync(BASE_PATH + name + '/index.js', `'use strict'
+
+import '@root/main'
+
+// Code
+`, { encoding: 'UTF-8' })
+
+/*
+ * ---------------------------------------------- Create required .scss file
+ */
+fs.writeFileSync(BASE_PATH + name + '/index.scss', `// noinspection CssUnknownTarget
+@import "@root/main.scss";
+`, { encoding: 'UTF-8' })
+
+/*
+ * ---------------------------------------------- Create required .html file
+ */
+fs.writeFileSync(BASE_PATH + name + '/index.html', `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <!-- START: Common part -->
+  <meta charset="utf-8">
+  <title><%= htmlWebpackPlugin.options._profile.name %></title>
+  <link rel="icon" href="/favicon.ico">
+  <meta property="og:title" content="Portfolio by <%= htmlWebpackPlugin.options._profile.name %>" />
+  <meta property="og:type" content="profile" />
+  <meta property="og:image" content="<%= htmlWebpackPlugin.options._profile.avatar_url %>" />
+  <meta property="og:url" content="https://<%= htmlWebpackPlugin.options._config.username %>.github.io/" />
+  <meta property="og:description" content="<%= htmlWebpackPlugin.options._profile.bio %>" />
+  <meta property="profile:username" content="<%= htmlWebpackPlugin.options._profile.login %>" />
+  <% for (let [property, content] of Object.entries(htmlWebpackPlugin.options._config.opg)) { %>
+    <meta property="<%= property %>" content="<%= content %>" />
+  <% } %>
+  <!-- END: Common part -->
+</head>
+<body>
+
+</body>
+</html>
+`, { encoding: 'UTF-8' })
+
+/*
+ * ---------------------------------------------- Complete
+ */
+console.log('Complete, path:', BASE_PATH + name)
+console.log('Change the template in the config.js file to:', name)
