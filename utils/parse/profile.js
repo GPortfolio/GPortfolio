d2d828ec801d14484d12a62e31a9a4edfc67f771
Alexey Khruhsch <alexeykhr@outlook.com>
2019-06-17 20:50:17 +0300

    refactor: improved utils folder, parse data (Cache class)

--- utils/parse/profile.js
+++ utils/parse/profile.js
@@ -1,22 +1,9 @@
 'use strict'
 
-const variables = require('../helper/variables')
-const jsonHelper = require('../helper/json')
+const variables = require('../variables')
+const Cache = require('../classes/Cache')
 const config = require('../../config')
 const axios = require('axios')
-
-/**
- * Waiting before the next request to the API.
- *  1 hour
- * @type {number} - milliseconds
- */
-const TIME_WAIT = 1000 * 60 * 60
-
-/**
- * How to save in timestamp
- * @type {string}
- */
-const CACHE_NAME = 'profile'
 
 /**
  * @return {Promise<?object>}
@@ -25,14 +12,12 @@ const CACHE_NAME = 'profile'
  */
 module.exports = async () => {
 
-  const timestamp = jsonHelper.tryReadJsonFile(variables.FILE_TIMESTAMP_JSON)
-  const profileFileExists = jsonHelper.existsJsonFile(variables.FILE_PROFILE_JSON)
-  const canRepeatRequestByTimestamp = (+timestamp[CACHE_NAME] || 0) + TIME_WAIT <= Date.now()
+  const cache = new Cache(variables.FILE_PROFILE_JSON)
 
   /** @type {?object} - data from API */
   let githubUser = null
 
-  if (canRepeatRequestByTimestamp || !profileFileExists) {
+  if (cache.canParse) {
 
     /**
      * Make a Github API request to get user data.
@@ -47,24 +32,20 @@ module.exports = async () => {
       })
       githubUser = fetchGithubData.data
       console.log(`[Profile] Complete, ${githubUser.name}`)
+
     } catch (e) {
       console.warn(`[Profile]: ${e}`)
       return null
     }
 
     /*
-     * Update cache
+     * Update cache and timestamp
      */
-    jsonHelper.writeJsonFile(variables.FILE_PROFILE_JSON, githubUser)
+    cache.updateData(githubUser)
+    cache.updateTimestamp()
 
-    /*
-     * Update timestamp
-     */
-    jsonHelper.writeJsonFile(variables.FILE_TIMESTAMP_JSON,
-      Object.assign({}, timestamp, { [CACHE_NAME]: Date.now() })
-    )
   } else {
-    githubUser = jsonHelper.tryReadJsonFile(variables.FILE_PROFILE_JSON)
+    githubUser = cache.fileData
     console.log(`[Profile] Get profile from cache, ${githubUser.name}`)
   }
 
